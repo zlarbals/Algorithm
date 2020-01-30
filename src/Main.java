64ee09d1a799bddef9f2938f96c3e422c0149319
zlarbals <zlarbals@gamil.com>
2020-01-31 03:50:50 +0900

    QuickSort Modified

--- src/Main.java
+++ src/Main.java
@@ -19,6 +19,6 @@ public class Main {
         }
         shuffle(arr);//random sort.
 
-        //test.
+        //test
     }
 }
--- src/QuickSort.java
+++ src/QuickSort.java
@@ -1,28 +1,25 @@
 public class QuickSort {
-    private int Partition(int[] array, int left, int right) {
-        int low = left + 1;
-        int high = right;
-        int value = array[left];
-        while (low<=high) {
-            while (value > array[low] && low <= right)
-                low++;
-            while (value < array[high] && high >= (left + 1))
-                high--;
+    public void sort(int[] array, int l, int r) {
+        int left=l;
+        int right=r;
+        int pivot = array[(left+right)/2];
 
-            if (low <= high)
-                Swap(array, low , high);
-        }
-        Swap(array, left, high);
-        return high;
-    }
+       do{
+           while(array[left]<pivot)left++;
+           while(array[right]>pivot)right--;
+           if(left<=right){
+               Swap(array,left,right);
+               left++;
+               right--;
+           }
+       }while(left<=right);
 
-    public void sort(int[] array, int left, int right) {
-        int middle;
-        if (left <= right) {
-            middle = Partition(array, left, right);
-            sort(array, left, middle - 1);
-            sort(array, middle + 1, right);
-        }
+        //return left,right 할 경우
+        //right pivotindex left 순으로 나열되어 있다면 left or right 중 하나를 리턴할 경우
+        //이미 정렬된 pivotindex부분도 다시 정렬 범위안에 들어가게 된다. 이것을 방지하고 싶다.
+        //따라서 일반적인 quick sort의 구조인 sort와 partiton으로 나누지 않고 sort에 재귀적으로 구현한다.
+        if(l<right) sort(array,l,right);
+        if(r>left) sort(array,left,r);
     }
 
     private void Swap(int[] arr, int changeIndex1, int changeIndex2) {
